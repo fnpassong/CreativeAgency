e8b577d257e88ad1627c091bccb593f6cfecf53c
fnpassong <fa_pa01@hotmail.com>
2024-01-14 17:54:51 -0500

    apprachData,se ingreso estilos a numbers.scss

---
+++ src/components/ApproachData.jsx
@@ -0,0 +1,25 @@
+
+
+const approachData = [
+
+    {
+        id:'01',
+        sub: 'Brand Strategy',
+        description: 'Brand strategy is critical for long-term success. Outshining competitors and capturing the target audience are key.'
+    },
+
+    {
+        id: '02',
+        sub: 'Brand Design',
+        description:'Keeping the brand design unique and meaningful helps in communicating the brand’s value effectively.' 
+    },
+
+    {
+        id: '03',
+        sub: 'Web Design',
+        description:'A beautifully crafted website is the best tool for brand awareness, and ultimately results in increased revenues.'
+    },
+
+]
+
+export default approachData;
--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+
+import approachData from './ApproachData';
+
+import '../scss/components/numbers.scss';
 
 export const Numbers = () => {
   return (
-    <div>Numbers</div>
+    <section className="numbers">
+        <div className="numbers__wrapper">
+            <h2 className="numbers__title">
+             Our approach for creating a winning brand
+            </h2>
+            <aside className="numbers__approach">
+                {
+                    approachData.map((data, index)=> {
+                        return(
+                            <div key={index} className="numbers__card">
+                                <section className='numbers__code'>
+                                    <h3 className='numbers__id'>{data.id}</h3>
+                                    <div className="numbers__sub">{data.sub}</div>
+                                </section>
+                                <p className="numbers__paragraph">
+                                    {data.description}
+                                </p>
+
+                            </div>
+                        )
+                    })
+                }
+
+            </aside>
+        </div>
+
+    </section>
   )
 }
