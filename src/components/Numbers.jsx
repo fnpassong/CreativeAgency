70ebfd594660d4a14b53f131bce4d405857c4dbd
fnpassong <fa_pa01@hotmail.com>
2024-01-15 21:41:55 -0500

    se ingreso <Numbers/> y sus estilo

--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -7,29 +7,35 @@ export const Numbers = () => {
   return (
     <section className="numbers">
         <div className="numbers__wrapper">
-            <h2 className="numbers__title">
-             Our approach for creating a winning brand
-            </h2>
-            <aside className="numbers__approach">
+
+            <h3 className="numbers__leftTitle">
+                Our approach for creating a winning brand
+            </h3> 
+
+            <div className="numbers__text">
+                <h3 className="numbers__title">
+                    Our approach for creating a winning brand
+                </h3>  
+
                 {
-                    approachData.map((data, index)=> {
+                    approachData.map((data,index) => {
                         return(
-                            <div key={index} className="numbers__card">
-                                <section className='numbers__code'>
-                                    <h3 className='numbers__id'>{data.id}</h3>
-                                    <div className="numbers__sub">{data.sub}</div>
-                                </section>
-                                <p className="numbers__paragraph">
+                            <article key={index} className="numbers__card">
+                                <div className="numbers__numberWrapper">
+                                   <div className='numbers__id'>{data.id}</div> 
+                                   <div className='numbers__sub'>{data.sub} </div> 
+                                </div>
+                                <p className='numbers__paragraph'>
                                     {data.description}
                                 </p>
-
-                            </div>
+                            </article>
                         )
                     })
                 }
 
-            </aside>
-        </div>
+            </div>
+
+        </div>    
 
     </section>
   )
